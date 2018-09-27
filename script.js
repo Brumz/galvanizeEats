4d40f615076ae1af056669e09b162e8071ea794b
Brumz <bmanrumsey24@gmail.com>
2018-09-27 15:21:35 -0600

    started js/populated menu items

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var newRequest = new XMLHttpRequest();
-newRequest.open('GET', ' https://galvanize-eats-api.herokuapp.com/menu');
-newRequest.onload = function() {
-    var menuData = JSON.parse(newRequest.responseText);
-    renderHTML(menuData);
-};
-newRequest.send();
+var menu = document.querySelector(".menuList")
+var option;
+var q = document.querySelector("#quantity")
+fetch('https:galvanize-eats-api.herokuapp.com/menu')
+   .then(function (response) {
+       return response.json();
+   })
+   .then(function(data) {
+       for ( let i = 0; i < data.menu.length; i++) {
+         option = document.createElement("p");
+         option.innerHTML = data.menu[i].name + "  " + "$" + +data.menu[i].price;
+         menu.appendChild(option);
+       }
 
-function renderHTML(data) {
-
-}
+   })
