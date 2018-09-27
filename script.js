a90dab2d5da9bd679a39dc43eec7b64a2c9cb42c
Brumz <bmanrumsey24@gmail.com>
2018-09-27 12:02:45 -0600

    started orders page

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var newRequest = new XMLHttpRequest();
 newRequest.open('GET', ' https://galvanize-eats-api.herokuapp.com/menu');
 newRequest.onload = function() {
-    console.log(newRequest.responseText);
+    var menuData = JSON.parse(newRequest.responseText);
+    renderHTML(menuData);
 };
 newRequest.send();
+
+function renderHTML(data) {
+
+}
