0ec814f2cf8fadd02374ba6ca51add9049c0518b
Brumz <bmanrumsey24@gmail.com>
2018-10-10 17:27:01 -0600

    populated the the sub total and the grand total with the price of all items and tax

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 window.onload = function gat() {
-    var menu = document.querySelector(".menuList");
     var item;
-    var option;
-    var q = document.querySelector("#quantity");
     var cB = document.querySelector(".cheeseB");
     var cP = document.querySelector(".cheeseP");
     var hB = document.querySelector(".hamB");
     var pP = document.querySelector(".pepP");
     var sP = document.querySelector(".suaP");
     var items = [];
-    var num = document.createElement("num");
     var addItem = document.querySelector("#addItem");
     var eats = document.querySelectorAll("#food");
-    // var totals = document.querySelector(".Totals")
-    var sub = document.querySelector("#Sub");
-    var subTax = document.querySelector("#Tax")
+    var cart = document.querySelector(".cartItems")
+    var cat = document.querySelector("#cats2");
+    var dog = document.querySelector("#dogs2");
+    var pet = document.querySelector("#pets");
+    var newArr = [];
     fetch('https:galvanize-eats-api.herokuapp.com/menu')
         .then(function (response) {
             return response.json();
@@ -30,36 +28,32 @@ window.onload = function gat() {
             hB.appendChild(items[2]);
             pP.appendChild(items[3]);
             sP.appendChild(items[4]);
-
         })
     addItem.addEventListener("click", function addItem(event) {
         for (let i = 0; i < eats.length; i++) {
-            var quantity = eats[i].children[0].value
+            var quantity = eats[i].children[0].value;
             if (quantity > 0) {
-                var foodInfo = eats[i].innerText
-                var foodName = foodInfo.split('$')[0].trim()
-                var foodPrice = Number(foodInfo.split('$')[1])
-                console.log(foodName, quantity, foodPrice)
-                // create element to append to order box (p tag)
-               var final = document.createElement("p");
-               var tax = document.createElement("p");
-                // give the value (innerText) to the element (p tag) with
-                    // Sausage Pizza x2 $25.98
-                    // food name
-                    // quantity
-                    // price timed by quantity
-                    final.innerText = foodName + " " + "x" + quantity + " " + "$" + foodPrice * quantity;
-                    // tax.innerText = foodPrice * quantity * 0.029;
-                // select order box
-                // append the element (p tag) to the order box
-                sub.appendChild(final);
-                // subTax.appendChild(tax);
-
-
+                var foodInfo = eats[i].innerText;
+                var foodName = foodInfo.split('$')[0].trim();
+                var foodPrice = Number(foodInfo.split('$')[1]);
+                var final = document.createElement("p");
+                final.setAttribute('value',foodPrice *quantity);
+                final.setAttribute('class','addedFoodItem');
+                final.innerText = foodName + " " + "x" + quantity + " " + "$" + foodPrice * quantity;
+                cart.appendChild(final);  
             }
         }
-
+        var addedFoodItem = document.getElementsByClassName("addedFoodItem");
+        for( let i = 0; i < addedFoodItem.length; i++) {
+            newArr.push(addedFoodItem[i].getAttribute("value"));
+        }
+        var itemTotal = 0;
+        for(i = 0;i < newArr.length; i++) {
+            itemTotal += parseFloat(newArr[i]);   
+        }
+        cat.innerHTML = ((itemTotal).toFixed(2));
+        dog.innerHTML = ((itemTotal * .03).toFixed(2));
+        pet.innerHTML = "$" + Number((parseFloat(cat.innerHTML) + parseFloat(dog.innerHTML)).toFixed(2));    
     })
 }
-
 
