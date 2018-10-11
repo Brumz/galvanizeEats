15ed3cf788c5b375a87a341e9881ecffecd07e82
Brumz <bmanrumsey24@gmail.com>
2018-10-11 09:46:14 -0600

    created POST request

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ window.onload = function gat() {
     var dog = document.querySelector("#dogs2");
     var pet = document.querySelector("#pets");
     var newArr = [];
+    var submitB = document.querySelector("#delivery");
     fetch('https:galvanize-eats-api.herokuapp.com/menu')
         .then(function (response) {
             return response.json();
@@ -37,23 +38,56 @@ window.onload = function gat() {
                 var foodName = foodInfo.split('$')[0].trim();
                 var foodPrice = Number(foodInfo.split('$')[1]);
                 var final = document.createElement("p");
-                final.setAttribute('value',foodPrice *quantity);
-                final.setAttribute('class','addedFoodItem');
+                final.setAttribute('value', foodPrice * quantity);
+                final.setAttribute('class', 'addedFoodItem');
                 final.innerText = foodName + " " + "x" + quantity + " " + "$" + foodPrice * quantity;
-                cart.appendChild(final);  
+                cart.appendChild(final);
             }
         }
         var addedFoodItem = document.getElementsByClassName("addedFoodItem");
-        for( let i = 0; i < addedFoodItem.length; i++) {
+        for (let i = 0; i < addedFoodItem.length; i++) {
             newArr.push(addedFoodItem[i].getAttribute("value"));
         }
         var itemTotal = 0;
-        for(i = 0;i < newArr.length; i++) {
-            itemTotal += parseFloat(newArr[i]);   
+        for (i = 0; i < newArr.length; i++) {
+            itemTotal += parseFloat(newArr[i]);
         }
         cat.innerHTML = ((itemTotal).toFixed(2));
         dog.innerHTML = ((itemTotal * .03).toFixed(2));
-        pet.innerHTML = "$" + Number((parseFloat(cat.innerHTML) + parseFloat(dog.innerHTML)).toFixed(2));    
+        pet.innerHTML = "$" + Number((parseFloat(cat.innerHTML) + parseFloat(dog.innerHTML)).toFixed(2));
     })
+    submitB.addEventListener("click", function () {
+        event.preventDefault()
+        var name = document.querySelector("#name")
+        var number = document.querySelector("#number")
+        var address = document.querySelector("#address")
+        var delivery = pet.innerHTML
+        var data = {
+            name: name,
+            number: number,
+            address: address,
+            delivery: delivery
+        }
+        var postURL = "https://galvanize-eats-api.herokuapp.com/orders"
+        var settings = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+        fetch(postURL, settings)
+            .then(function (response) {
+                return response.text()
+            })
+            .then(function (response) {
+                alert(response + " " + "Your order has been placed")
+            })
+            .then(function () {
+                location.reload()
+            })
+    })
+
+
 }
 
